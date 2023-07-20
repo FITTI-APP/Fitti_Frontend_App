9a73a2227adb8abdd3c2b4c350ec97ea341d80af
dicohy27 <jungjuen0927@naver.com>
2023-07-20 23:51:50 +0900

    feat: ExerciseListPage에서 운동 클릭하면 DailyRoutinePage에 운동 추가되는 기능 구현 완료 todo: 운동 삭제 기능

---
+++ fitty_frontend_app/lib/domain/plan/exercise/exercise_widget.dart
@@ -0,0 +1,23 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class ExerciseWidget extends StatelessWidget {
+  const ExerciseWidget({
+    super.key,
+    required this.exerciseName,
+    required this.index,
+  });
+  final String exerciseName;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(exerciseName),
+      onTap: () {
+        Navigator.pop(context, exerciseName);
+      },
+    );
+  }
+}
