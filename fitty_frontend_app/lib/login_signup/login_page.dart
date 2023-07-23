f5ba53cd94bde39a6d7a9eaa3825926f2282dde4
JJolggit <2019100679@khu.ac.kr>
2023-07-24 01:38:50 +0900

    안녕하세요

---
+++ fitty_frontend_app/lib/login_signup/login_page.dart
@@ -0,0 +1,152 @@
+import 'package:fitty_frontend_app/login_signup/signup_page.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  var formKey = GlobalKey<FormState>();
+  var emailController = TextEditingController();
+  var passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.fitness_center_outlined,
+                  size: 100,
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  'F I T T Y',
+                  style: GoogleFonts.bebasNeue(fontSize: 60.0),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  '당신의 작은 피트니스 친구',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            border: Border.all(color: Colors.white),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 20.0),
+                            child: TextFormField(
+                              controller: emailController,
+                              validator: (val) =>
+                                  val == "" ? "Please enter email" : null,
+                              decoration: InputDecoration(
+                                  border: InputBorder.none, hintText: '이메일'),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.grey[200],
+                              border: Border.all(color: Colors.white),
+                              borderRadius: BorderRadius.circular(12)),
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 20.0),
+                            child: TextFormField(
+                              controller: passwordController,
+                              validator: (val) =>
+                                  val == "" ? "Please enter password" : null,
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                  border: InputBorder.none, hintText: '비밀번호'),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 25.0),
+                      child: Container(
+                        padding: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 25, 42, 173),
+                            borderRadius: BorderRadius.circular(12)),
+                        child: Center(
+                          child: Text(
+                            '로그인',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('우리의 친구가 아니십니까?'),
+                    GestureDetector(
+                      onTap: () => Get.to(() => const SignupPage()),
+                      child: Text(
+                        ' 지금 가입하세요!',
+                        style: TextStyle(
+                            color: Colors.blue, fontWeight: FontWeight.bold),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- fitty_frontend_app/lib/main.dart
+++ fitty_frontend_app/lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:fitty_frontend_app/login_signup/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'bottom_navigation_page.dart';
+import 'login_signup/login_page.dart';
 
 void main() async {
-  await initializeDateFormatting();
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BottomNavigationPage(),
+      home: LoginPage(),
     );
   }
 }
