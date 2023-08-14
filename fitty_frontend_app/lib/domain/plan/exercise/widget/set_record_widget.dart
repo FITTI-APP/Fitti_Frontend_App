f67c96e70473a71edf564c592b50b62a8140d372
uuccu <sungu0804@gmail.com>
2023-08-14 18:29:05 +0900

    feat: 숫자만 입력가능하게 하기 4자리까지.

--- fitty_frontend_app/lib/domain/plan/exercise/widget/set_record_widget.dart
+++ fitty_frontend_app/lib/domain/plan/exercise/widget/set_record_widget.dart
@@ -1,5 +1,7 @@
 import 'package:fitty_frontend_app/domain/plan/exercise/all_exercise_record.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class SetRecordWidget extends StatelessWidget {
   SetRecordWidget({
@@ -30,32 +32,44 @@ class SetRecordWidget extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
         SizedBox(
-          width: 60,
+          width: 65,
           height: 25,
           child: TextField(
             controller: kgController,
             textAlign: TextAlign.center,
+            keyboardType: TextInputType.number,
+            inputFormatters: [
+              FilteringTextInputFormatter.allow(RegExp(r'[1-9]\d{0,3}'))
+            ],
+            maxLength: 4,
             onChanged: (value) => {
               oneSetInfo.kg = int.parse(value),
               updateExerciseRecords(),
             },
             decoration: const InputDecoration(
+              counterText: '',
               border: OutlineInputBorder(),
             ),
           ),
         ),
         const Text('kg'),
         SizedBox(
-          width: 55,
+          width: 65,
           height: 25,
           child: TextField(
             controller: repsController,
             textAlign: TextAlign.center,
+            keyboardType: TextInputType.number,
+            inputFormatters: [
+              FilteringTextInputFormatter.allow(RegExp(r'[1-9]\d{0,3}'))
+            ],
+            maxLength: 4,
             onChanged: (value) => {
               oneSetInfo.reps = int.parse(value),
               updateExerciseRecords(),
             },
             decoration: const InputDecoration(
+              counterText: '',
               border: OutlineInputBorder(),
             ),
           ),
