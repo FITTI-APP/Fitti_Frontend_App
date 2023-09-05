d1e6f096f329a460621af7aafb7b62d1658c2974
dicohy27 <59111693+dicohy27@users.noreply.github.com>
2023-09-05 13:46:14 +0900

    API클래스 수정

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,11 +1,10 @@
 class API {
-  static const hostConnect = "http://192.168.0.41/api_new_members";
+  static const hostConnect =
+      "http://fitty-develop-api.ap-northeast-2.elasticbeanstalk.com";
   static const hostConnectUser = "$hostConnect/user";
 
   static const signup = "$hostConnect/user/signup.php";
   static const validateEmail = "$hostConnect/user/validate_email.php";
-  static const login =
-      "http://fitty-develop-api.ap-northeast-2.elasticbeanstalk.com/auth/login";
-  static const register =
-      "http://fitty-develop-api.ap-northeast-2.elasticbeanstalk.com/auth/register";
+  static const login = "$hostConnect/auth/login";
+  static const register = "$hostConnect/auth/register";
 }
