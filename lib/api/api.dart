028427015e48f256a42456d076f35c36ed1f0c12
dicohy27 <59111693+dicohy27@users.noreply.github.com>
2023-09-05 11:37:33 +0900

    login, signup api 주소

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -4,4 +4,8 @@ class API {
 
   static const signup = "$hostConnect/user/signup.php";
   static const validateEmail = "$hostConnect/user/validate_email.php";
+  static const login =
+      "http://fitty-develop-api.ap-northeast-2.elasticbeanstalk.com/auth/login";
+  static const register =
+      "http://fitty-develop-api.ap-northeast-2.elasticbeanstalk.com/auth/register";
 }
