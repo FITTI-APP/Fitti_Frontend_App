c77459f46084462a9d0a60055e689bcd26dd1c3b
dicohy27 <59111693+dicohy27@users.noreply.github.com>
2023-10-03 01:18:19 +0900

    MyDietHomePage new UI

--- lib/page/diet/my_diet_home_page.dart
+++ lib/page/diet/my_diet_home_page.dart
@@ -1,6 +1,6 @@
-import 'package:fitti_frontend_app/page/diet/diet_edit_page.dart';
+import 'package:fitti_frontend_app/widget/common/today_start_widget.dart';
+import 'package:fitti_frontend_app/widget/common/week_calendar_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class MyDietHomePage extends StatefulWidget {
   const MyDietHomePage({super.key});
@@ -10,48 +10,58 @@ class MyDietHomePage extends StatefulWidget {
 }
 
 class _MyDietHomePageState extends State<MyDietHomePage> {
-  List<String> dietList = <String>['아침', '점심', '저녁', '간식'];
-  String dietTitle = '아침';
   @override
   Widget build(BuildContext context) {
+    DateTime now = DateTime.now();
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("My 식단"),
-        ),
-        body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              DropdownButton<String>(
-                value: dietTitle,
-                icon: const Icon(Icons.arrow_downward),
-                style: const TextStyle(color: Colors.black),
-                underline: Container(
-                  height: 2,
-                  color: Colors.blue,
-                ),
-                onChanged: (String? value) {
-                  setState(() {
-                    dietTitle = value!;
-                  });
-                },
-                items: dietList.map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Get.to(
-                    () => DietEditPage(title: dietTitle),
-                  );
-                },
-                child: const Text('식단 추가'),
-              )
-            ],
+      appBar: AppBar(
+        title: const Text("My 식단"),
+      ),
+      body: Column(
+        children: [
+          const Text("OOO님의 식단 분석"),
+          TodayStartWidget(
+            now: now,
+            buttonText: "오늘 식단 추가하기",
+            nextPage: const Text("오늘 식단 추가하기"),
           ),
-        ));
+          const SizedBox(
+            height: 10,
+          ),
+          WeekCalendarWidget(),
+          Card(
+            child: SizedBox(
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        width: 100,
+                        height: 100,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.blue,
+                        ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          children: [
+                            Text("오늘 섭취량"),
+                            Text("2350 Kcal"),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  Divider(),
+                  Text("현재 비율"),
+                  Text("목표 비율"),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
