18a55a2597c4e47dcdb4e223c6f4e85d8ff576c3
정재현 <59111693+dicohy27@users.noreply.github.com>
2024-02-01 00:27:32 +0900

    로그인 페이지 디자인만(간편 로그인 전까지 함)

--- lib/page/login_signup/new_login_page.dart
+++ lib/page/login_signup/new_login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class NewLoginPage extends StatelessWidget {
   @override
@@ -10,54 +11,202 @@ class NewLoginPage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(
-                'FITTI',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+              SizedBox(
+                width: 212.w,
+                child: Column(
+                  children: [
+                    Text(
+                      'FITTI',
+                      style: TextStyle(
+                        fontSize: 40.sp,
+                        fontWeight: FontWeight.w500,
+                        fontFamily: 'Roboto Condensed',
+                      ),
+                    ),
+                    SizedBox(height: 39.w),
+                    SizedBox(
+                      width: 212.w,
+                      height: 34.h,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                            borderSide: BorderSide(width: 0.5),
+                          ),
+                          labelText: 'ID',
+                          labelStyle: TextStyle(
+                            fontSize: 12.sp,
+                            fontWeight: FontWeight.w400,
+                            fontFamily: 'Roboto',
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 7.h),
+                    SizedBox(
+                      width: 212.w,
+                      height: 34.h,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                            borderSide: BorderSide(
+                              width: 0.5,
+                            ),
+                          ),
+                          labelText: 'PSW',
+                          labelStyle: TextStyle(
+                            fontSize: 12.sp,
+                            fontWeight: FontWeight.w400,
+                            fontFamily: 'Roboto',
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 8.w),
+                    Row(
+                      children: [
+                        SizedBox(
+                          // Checkbox 실제 사이즈가 18X18 인데 패딩이 커서 고정
+                          height: 18,
+                          width: 18,
+                          child: Checkbox(
+                            value: false,
+                            onChanged: (value) {},
+                            side: BorderSide(width: 0.5),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(0),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 6.w),
+                        Text(
+                          '로그인 유지',
+                          style: TextStyle(
+                            fontSize: 10.sp,
+                            fontFamily: 'Roboto',
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 19.h),
+                    SizedBox(
+                      width: 212.w,
+                      height: 34.h,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          '로그인',
+                          style: TextStyle(
+                            fontSize: 13.sp,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: 'Roboto',
+                            color: Colors.white,
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          elevation: 0.0,
+                          backgroundColor: Colors.black,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              SizedBox(height: 16),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'ID',
-                ),
-              ),
-              SizedBox(height: 16),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: '비밀번호',
-                ),
-              ),
-              SizedBox(height: 16),
-              Row(
-                children: [
-                  Checkbox(
-                    value: false,
-                    onChanged: (value) {},
-                  ),
-                  Text('로그인 유지'),
-                ],
-              ),
-              SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text('로그인'),
-              ),
-              SizedBox(height: 16),
+              SizedBox(height: 45.h),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   TextButton(
                     onPressed: () {},
-                    child: Text('회원가입'),
+                    style: TextButton.styleFrom(
+                      minimumSize: Size.zero,
+                      padding: EdgeInsets.zero,
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    ),
+                    child: Text(
+                      '회원가입',
+                      style: TextStyle(
+                        fontSize: 10.sp,
+                        fontWeight: FontWeight.w400,
+                        fontFamily: 'Roboto',
+                        decoration: TextDecoration.underline,
+                        color: Colors.black,
+                      ),
+                    ),
                   ),
-                  SizedBox(width: 16),
+                  SizedBox(
+                    width: 10.w,
+                  ),
+                  SizedBox(
+                    height: 10.h,
+                    child: VerticalDivider(
+                      width: 1,
+                      thickness: 1,
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10.w,
+                  ),
                   TextButton(
                     onPressed: () {},
-                    child: Text('찾기'),
+                    style: TextButton.styleFrom(
+                      minimumSize: Size.zero,
+                      padding: EdgeInsets.zero,
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    ),
+                    child: Text(
+                      '찾기',
+                      style: TextStyle(
+                        fontSize: 10.sp,
+                        fontWeight: FontWeight.w400,
+                        fontFamily: 'Roboto',
+                        decoration: TextDecoration.underline,
+                        color: Colors.black,
+                      ),
+                    ),
                   ),
                 ],
+              ),
+              SizedBox(height: 7.h),
+              SizedBox(
+                width: 262.w,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 1,
+                        color: Color(0xFF848484),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 3.w,
+                    ),
+                    Text(
+                      '간편 로그인',
+                      style: TextStyle(
+                        fontSize: 10.sp,
+                        fontWeight: FontWeight.w400,
+                        fontFamily: 'Roboto',
+                        color: Color(0xFF848484),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 3.w,
+                    ),
+                    Expanded(
+                      child: Divider(
+                        thickness: 1,
+                        color: Color(0xFF848484),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
