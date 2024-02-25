ffcab9897d96e8f71b657b9902e315ea3a1d573f
정재현 <59111693+dicohy27@users.noreply.github.com>
2024-02-25 18:08:18 +0900

    컬러 상수 수정

--- lib/style/colors.dart
+++ lib/style/colors.dart
@@ -1,5 +1,3 @@
 import 'package:flutter/material.dart';
 
-const Color blackColor = Color(0xFF000000);
-const Color grayColor = Color(0xFF848484);
-const Color whiteColor = Color(0xFFFFFFFF);
+const Color greyColor = Color(0xFF848484);
