acb84476eb7e595f7ba8fc9cf5838607bfd7779c
uuccu <sungu0804@gmail.com>
2024-04-23 00:51:21 +0900

    Update colors.dart

--- lib/style/colors.dart
+++ lib/style/colors.dart
@@ -5,5 +5,7 @@ const Color shadowColor = Color(0x3F000000);
 const Color skyBlueColor = Color(0xFF82D9FF);
 const Color greenColor = Color(0xFF0AA00D);
 const Color redColor = Color(0xFFFF543C);
+const Color setDeleteRedColor = Color.fromARGB(255, 200, 0, 0);
+const Color finishExerciseButtonColor = Color.fromARGB(255, 149, 0, 0);
 const Color blueColor = Color(0xFF4C53E5);
 const Color startButtonColor = Color.fromARGB(255, 242, 253, 255);
