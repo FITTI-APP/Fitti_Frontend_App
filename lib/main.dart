f2644b620f29bcbf5a4c208772800a7115dfbac0
uuccu <sungu0804@gmail.com>
2024-04-25 21:21:48 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -112,7 +112,7 @@ Widget _splashLoadingWidget(AsyncSnapshot snapshot) {
       return MenuRoutingPage(token: token);
     } else {
       // not logged in (token does not exist)
-      return const MenuRoutingPage(token: "");
+      return const LoginPage();
     }
   } else {
     return const IntroPage();
